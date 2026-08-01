91a21a559bf8254fed2609bd113206519af821c2
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-08-02 01:05:48 +0530

    Update the Program and made by taking the input from the user

--- LECTURES/2(Conditional loop + Calculator Program)/Counting_Occurence.java
+++ LECTURES/2(Conditional loop + Calculator Program)/Counting_Occurence.java
@@ -1,30 +1,50 @@
+import java.util.Scanner;
+
 public class Counting_Occurence{
 
 public static void main(String[] args) {
 
     // Without Taking Input // 
 
-    int n = 523363;
-   int count = 0 ;
+//     int n = 523363;
+//    int count = 0 ;
 
 
-   while (n > count) {
+//    while (n > count) {
 
-    int rem = n % 10 ;
-    if(rem == 3 ){
-        count++ ;
-    }
-    n = n /10;
+//     int rem = n % 10 ;
+//     if(rem == 3 ){
+//         count++ ;
+//     }
+//     n = n /10;
 
-   }
-   System.out.println(count);
+//    }
+//    System.out.println(count);
 
 
   // WithTaking Input //
 
-  
+  Scanner in = new Scanner(System.in);
+
+  System.out.println("Enter the Numbers :");
+  int n= in.nextInt();
+  int count[] = new int[10];
 
 
+  while (n > 0){
+
+    int rem = n % 10;
+    count[rem]++;
+    n = n / 10;
+  }
+
+  System.out.println("Frequency of digits:");
+
+        for (int i = 0; i < 10; i++) {
+            if (count[i] > 0) {
+                System.out.println(i + " -> " + count[i] + " times");
+            }
+        }
 
 }
 
