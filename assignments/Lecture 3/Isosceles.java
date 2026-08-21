7cee935871425d72c6440bd9ffa9e80009481d7f
Aditya Vaishnav <Aditya1807.arya@gmail.com>
2026-08-22 01:46:43 +0530

    create Area of Isosceles Triangle program

--- assignments/Lecture 3/Isosceles.java
+++ assignments/Lecture 3/Isosceles.java
@@ -5,6 +5,22 @@ public class Isosceles{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("");
+        System.out.print("Enter the Base: ");
+        double base = in.nextDouble();
+        System.out.print("Ente the Side :");
+        double side = in.nextDouble();
+
+        double height =(Math.sqrt(side * side  -(base * base )/4));
+
+        if(base > 0 && height > 0){
+            double Area = (0.5 * base * height);
+
+            System.out.println("Area of Isosceles Triangle: " + Area);
+        }
+        else{
+            System.out.println("Base and Height always be Positive");
+        }
+
+
     }
 }
